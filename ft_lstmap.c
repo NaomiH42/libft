6cae24a08385c8584cb0581574e7dd2b4f603035
NaomiH42 <naomi.hasalova@gmail.com>
2023-01-14 11:02:25 +0100

    Ok, maybe all done

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: ehasalu <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/13 12:39:03 by ehasalu           #+#    #+#             */
-/*   Updated: 2023/01/13 12:42:33 by ehasalu          ###   ########.fr       */
+/*   Updated: 2023/01/14 11:02:06 by ehasalu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,21 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
+	t_list	*head;
 	t_list	*temp;
 
-	temp = NULL;
-	while (lst)
+	head = NULL;
+	if (!lst || !del || !f)
+		return (NULL);
+	head = ft_lstnew((f)(lst->content));
+	temp = head;
+	while (lst->next != NULL)
 	{
-		temp = (t_list *)malloc(sizeof(t_list));
-		temp = (f)(lst->content);
 		lst = lst->next;
+		temp->next = ft_lstnew((f)(lst->content));
+		if (!temp)
+			(del)(lst);
 		temp = temp->next;
 	}
+	return (head);
 }
