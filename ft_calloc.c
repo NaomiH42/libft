9b9abc9e1da2a29a34c694cfd290ac2fee271c6f
Evelyn Hasalu <ehasalu@c2r9s7.42prague.com>
2023-01-20 14:08:48 +0100

    Finished libft

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: ehasalu <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/11 14:57:30 by ehasalu           #+#    #+#             */
-/*   Updated: 2023/01/17 10:43:36 by ehasalu          ###   ########.fr       */
+/*   Updated: 2023/01/17 14:10:49 by ehasalu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,11 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	void	*temp;
 	size_t	test;
 
-	if (nmemb == 0 && size == 0)
+	if (nmemb == 0 || size == 0)
 	{
 		temp = malloc(0);
 		return (temp);
 	}
-	else if (nmemb == 0 || size == 0)
-		return (NULL);
 	test = nmemb * size;
 	if (test / nmemb != size)
 		return (NULL);
